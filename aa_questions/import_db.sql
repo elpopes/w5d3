3bf8f01774947d8f288c8202c09c7dfaf3e36b3a
lorenzo <lorenzotijerina@gmail.com>
2022-12-07 11:05:36 -0500

    creating tables latest

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -17,6 +17,20 @@ CREATE TABLE questions (
 );
 
 CREATE TABLE question_follows (
-    id INTEGER PRIMARY KEY,
-    user
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+    question_id INTEGER NOT NULL,
+    -- EACH REPLY REF PARENT REPLY insert here
+    user_id INTEGER NOT NULL,
+    
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+
 );
