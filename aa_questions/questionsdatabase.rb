0e4ac20482c3999bef68984d45a8d99759ffe5eb
Logan Hartman <loganchart@gmail.com>
2022-12-07 15:19:34 -0500

    completed Question Class

--- aa_questions/questionsdatabase.rb
+++ aa_questions/questionsdatabase.rb
@@ -12,6 +12,32 @@ class QuestionsDataBase < SQLite3::Database
 end
 require "byebug"
 class User
+
+    def self.find_by_id(id)
+        user_hash = QuestionsDataBase.instance.execute(<<-SQL, id)
+
+            SELECT 
+                *
+            FROM
+                users
+            WHERE
+                id = ?;
+        SQL
+        User.new(user_hash.first)
+    end
+
+    def self.find_by_name(fname, lname)
+        user_hash = QuestionsDataBase.instance.execute(<<-SQL, fname, lname)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+            fname = ? AND lname = ?;
+        SQL
+
+        User.new(user_hash.first)
+    end
     
     attr_accessor :id, :fname, :lname
     
@@ -33,22 +59,31 @@ class User
         id = QuestionsDataBase.instance.last_insert_row_id
     end
 
-    def self.find_by_id(id)
-        user_hash = QuestionsDataBase.instance.execute(<<-SQL, id)
 
-            SELECT 
-                *
-            FROM
-                users
-            WHERE
-                id = ?;
-        SQL
-        User.new(user_hash.first)
-    end
 
 end
 
 class Question
+
+    attr_accessor :id, :title, :body, :author_id # TR
+
+    def initialize(options)
+        @id = options["id"]
+        @title = options["title"]
+        @body = options["body"]
+        @author_id = options["author_id"]
+    end
+
+    def create                      # TEST WITHOUT @ for parameters becasue attr_accessor
+        raise "#{self} is already in database" if @id
+        QuestionsDataBase.instance.execute(<<-SQL, @title, @body, @author_id)
+            INSERT INTO
+                questions (title, body, author_id)
+            VALUES
+                (?, ?, ?)
+            SQL
+        id = QuestionsDataBase.instance.last_insert_row_id
+    end
 
 end
 
