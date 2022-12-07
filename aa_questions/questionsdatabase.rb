72b57191a1081103473fd2af199cb74b77710834
lorenzo <lorenzotijerina@gmail.com>
2022-12-07 14:54:58 -0500

    complete find_by_id

--- aa_questions/questionsdatabase.rb
+++ aa_questions/questionsdatabase.rb
@@ -10,12 +10,13 @@ class QuestionsDataBase < SQLite3::Database
         self.results_as_hash = true
     end
 end
-
+require "byebug"
 class User
     
     attr_accessor :id, :fname, :lname
-
+    
     def initialize(options)
+        # debugger
         @id = options['id']
         @fname = options['fname']
         @lname = options['lname']
@@ -23,13 +24,26 @@ class User
 
     def create
         raise "#{self} already in database" if @id
-        QuestionsDataBase.instance.execute(<<-SQL, @fname, @lastname)
+        QuestionsDataBase.instance.execute(<<-SQL, @fname, @lname)
             INSERT INTO
                 users (fname, lname)
             VALUES
                 (?, ?)
         SQL
         id = QuestionsDataBase.instance.last_insert_row_id
+    end
+
+    def self.find_by_id(id)
+        user_hash = QuestionsDataBase.instance.execute(<<-SQL, id)
+
+            SELECT 
+                *
+            FROM
+                users
+            WHERE
+                id = ?;
+        SQL
+        User.new(user_hash.first)
     end
 
 end
