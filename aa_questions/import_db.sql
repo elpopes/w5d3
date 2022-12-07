c115c331ec8772067e1def7a4f2cfbb7ad2d5614
lorenzo <lorenzotijerina@gmail.com>
2022-12-07 11:01:51 -0500

    creating tables

---
+++ aa_questions/import_db.sql
@@ -0,0 +1,22 @@
+-- start here
+PRAGMAforeign_keys = ON
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL, 
+    lname TEXT NOT NULL,
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user
+);
